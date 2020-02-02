1de0a1174ec17d075756c1dacd366f40d1b12a3f
Harley McDowell <hmcdowell95@gmail.com>
2020-02-02 19:15:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,6 +39,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash.collect do |k, v|
     if k == season
+      binding.pry
       holiday_hash[k][holiday_name => supply_array]
     end
   end
